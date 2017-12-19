f3b42f628d33908bded1c11aacdd173a17694881
git-sujit <sujit.cse.nitt@gmail.com>
2017-12-19 12:47:31 -0800

    Zipkin/Rabbit MQ

---
+++ src/main/java/com/sks/learn/microservices/zipkindistributedtracingserver/ZipkinDistributedTracingServerApplication.java
@@ -0,0 +1,17 @@
+package com.sks.learn.microservices.zipkindistributedtracingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@EnableDiscoveryClient
+public class ZipkinDistributedTracingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDistributedTracingServerApplication.class, args);
+	}
+}
